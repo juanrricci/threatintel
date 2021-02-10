c81b409153764689bf3c936c4423c3a217f6fdef
Juan Ricci <juanrricci@gmail.com>
2021-02-10 18:13:10 -0300

    Add predecoder regular expression

--- scripts/normalizer-decoders/src/predecoder.py
+++ scripts/normalizer-decoders/src/predecoder.py
@@ -9,6 +9,7 @@ def formatByType(type):
     return format.get(type, 'Invalid log type')
 
 def predecode(log):
-    m = re.search('^(?P<type>\d):', log)
+    m = re.search('^(?P<type>\d):\[(?P<someid>\d+)\] \((?P<agentname>[\w-]+)\) (?P<ip>\d+\.\d+\.\d+\.\d+):(?P<port>\d+):(?P<path>[\/\w-]+):(?P<rawlog>.+)', log)
+    print('M:', m.groups())
     
     return formatByType(m.group('type'))
