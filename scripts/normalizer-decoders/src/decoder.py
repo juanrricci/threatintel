dafca1cb48208e97760fbaaa007dfa9f86bcccd6
Juan Ricci <juanrricci@gmail.com>
2021-02-12 12:46:32 -0300

    Choose JSON decoder if matches all fields

--- scripts/normalizer-decoders/src/decoder.py
+++ scripts/normalizer-decoders/src/decoder.py
@@ -1,23 +1,27 @@
 from benedict import benedict
 
-def checkPrematchConditions(rawLog, matchedDecoders):
+def checkFields(benedictedLog, decoderFields):
+    for field in decoderFields:
+        print('\nChecking field', field)
+        if not field in benedictedLog:
+            print('\nField', field, 'is invalid. Decoder invalid too.')
+            return False
+        else:
+            print('\nField', field, 'is valid. Checking the following one...')
+    print('\nAll fields are valid. Matched decoder.')
+    return True
+
+def checkMatchedDecoders(rawLog, matchedDecoders):
     benedictedLog = benedict(rawLog)
     for decoder in matchedDecoders:
-        for field in decoder['prematch']['has_field']:
-            if not field in benedictedLog:
-                print('\nPrematch field', field, 'not found.')
-                break
-                # Finish the nested loop for continuing checking the following decoder fields
-            else: print('Prematch field', field, 'found. OK.')
-        # If all fields of the decoder match, this is the proper decoder
-        return True
-    # If all decoders fail, return False
+        print('\nChecking decoder', decoder['filename'])
+        if checkFields(benedictedLog, decoder['prematch']['has_field']):
+            print('\nMatched decoder:', decoder['filename'])
+            return decoder['filename']
+    print('\nNone of listed decoders have matched.')
     return False
 
 def decode(predecodedLog, decodersByFormat):
     matchedDecoders = decodersByFormat[predecodedLog['log']['type']]
     print('\nMatched JSON Decoders:', matchedDecoders)
-    print('\nPrematch result:', checkPrematchConditions(predecodedLog['log']['raw'], decodersByFormat[predecodedLog['log']['type']]))
-    # applyMatchedDecoders(matchedDecoders, predecodedLog.get('log').get('raw'))
-
-    # return predecodedLog
+    print('\nPrematch result:', checkMatchedDecoders(predecodedLog['log']['raw'], decodersByFormat[predecodedLog['log']['type']]))
