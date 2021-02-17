31a5180df03c08a064ff8b1aeec1e867fefc8096
Juan Ricci <juanrricci@gmail.com>
2021-02-17 20:45:06 -0300

    Add checkRegex function

--- scripts/normalizer-decoders/src/prematch.py
+++ scripts/normalizer-decoders/src/prematch.py
@@ -1,8 +1,9 @@
+import re
 from benedict import benedict
 from pprint import pprint
 
-def checkFields(benedictedLog, decoderFields):
-    for field in decoderFields:
+def checkFields(benedictedLog, prematchFields):
+    for field in prematchFields:
         print('-- Checking field', field)
         if not field in benedictedLog:
             print('* Field', field, 'is invalid. Decoder invalid too.')
@@ -12,26 +13,41 @@ def checkFields(benedictedLog, decoderFields):
     print('* All fields are valid. Matched decoder.')
     return True
 
+def checkRegex(rawLog, prematchRegex):
+    for regex in prematchRegex:
+        print('-- Checking regex', regex)
+        if not re.match(regex, rawLog):
+            print('* Regex', regex, 'is invalid. Decoder invalid too.')
+            return False
+        else:
+            print('--- Regex', regex, 'is valid. Checking the following one...')
+    print('* All fields are valid. Matched decoder.')
+    return True
+
 def checkJSONDecoders(rawLog, matchedDecoders):
     benedictedLog = benedict(rawLog)
     for decoder in matchedDecoders:
         print('\n* Checking decoder', decoder['filename'])
         if checkFields(benedictedLog, decoder['prematch']['has_field']):
-            print('\nMatched decoder:', decoder['filename'])
+            print('\nMatched decoder:', decoder['filename'], 'by prematch fields.')
             return decoder['filename']
     print('\n* None of listed decoders have matched.')
     return False
 
 def checkPlaintextDecoders(rawLog, matchedDecoders):
-    print('Plaintext decoders must be checked here.')
-    print(matchedDecoders)
-    print(rawLog)
+    for decoder in matchedDecoders:
+        print('\n* Checking decoder', decoder['filename'])
+        if checkRegex(rawLog, decoder['prematch']['regex']):
+            print('\nMatched decoder:', decoder['filename'], 'by prematch regex.')
+            return decoder['filename']
+    print('\n* None of listed decoders have matched.')
+    return False
 
 def checkXMLDecoders(rawLog, matchedDecoders):
     print('XML decoders must be checked here.')
 
 def prematch(predecodedLog, decodersByFormat):
-    print('\nMatched JSON Decoders:') 
+    print('\nMatched Decoders:') 
     pprint(decodersByFormat[predecodedLog['log']['type']])
     formatsOfDecoders = {
         'json': checkJSONDecoders,
